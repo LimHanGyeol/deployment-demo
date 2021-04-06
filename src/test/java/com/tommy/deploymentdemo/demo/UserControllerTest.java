3a666ad7c25d6074255ab41ae25fdee7170dd472
임한결 <dlagksruf19@naver.com>
2021-04-06 11:52:28 +0900

    revert UserControllerTest

---
+++ src/test/java/com/tommy/deploymentdemo/demo/UserControllerTest.java
@@ -0,0 +1,29 @@
+package com.tommy.deploymentdemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getUser() throws Exception {
+        mockMvc.perform(get("/")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Hangyeol"));
+    }
+
+}
