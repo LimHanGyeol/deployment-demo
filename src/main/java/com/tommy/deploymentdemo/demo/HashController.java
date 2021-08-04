97014afc5254962fdba1a44d363f32e61deadb30
limhangyeol <dlagksruf19@naver.com>
2021-08-04 16:01:48 +0900

    feat : 테스트용 컨트롤러 생성

---
+++ src/main/java/com/tommy/deploymentdemo/demo/HashController.java
@@ -0,0 +1,19 @@
+package com.tommy.deploymentdemo.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HashController {
+
+    @GetMapping("/hash/{id}")
+    public String getDigest(@PathVariable("id") String id) {
+        return id;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "deploy";
+    }
+}
