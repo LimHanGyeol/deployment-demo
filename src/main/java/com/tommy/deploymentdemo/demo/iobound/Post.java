0767d3299a2592da0ff0625360c7e33d5d140fc5
limhangyeol <dlagksruf19@naver.com>
2021-08-06 14:54:22 +0900

    feat : Postgresql 설정

--- src/main/java/com/tommy/deploymentdemo/demo/iobound/Post.java
+++ src/main/java/com/tommy/deploymentdemo/demo/iobound/Post.java
@@ -8,8 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Post {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String content;
