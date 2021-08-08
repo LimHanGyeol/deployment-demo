6b454c8b2da3c5231b9cd6b2c309f45127b7448a
limhangyeol <dlagksruf19@naver.com>
2021-08-08 16:03:27 +0900

    refactor : dev Profile, TestCode 수정
    testcode의 @ActiveProfiles 설정을 해주지 않아 yaml의 Property를 가져오지 못했다.
    해당 프로젝트보다 큰 규모로 개발할 경우 test profile을 만들어 방지를 염두해야 할 것이다.

--- src/test/java/com/tommy/deploymentdemo/demo/DemoApplicationTests.java
+++ src/test/java/com/tommy/deploymentdemo/demo/DemoApplicationTests.java
@@ -2,7 +2,9 @@ package com.tommy.deploymentdemo.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("dev")
 @SpringBootTest
 class DemoApplicationTests {
 
--- src/test/java/com/tommy/deploymentdemo/demo/UserControllerTest.java
+++ src/test/java/com/tommy/deploymentdemo/demo/UserControllerTest.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("dev")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserControllerTest {
